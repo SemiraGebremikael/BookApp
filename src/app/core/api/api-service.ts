23945f92460e00a18a7486dc273cdf349d113c66
Semira gebremikael <semiragere61@gmail.com>
2026-05-18 01:58:48 +0200

    Fix CORS configuration for Angular frontend

--- src/app/core/api/api-service.ts
+++ src/app/core/api/api-service.ts
@@ -4,65 +4,66 @@ import { HttpClient } from '@angular/common/http';
 import { CreateQuoteDto, QuoteDto, UpdateQuoteDto } from '../models/quotes/quote.dto';
 import { RegisterDto } from '../models/auth/register.dto';
 import { LoginDto } from '../models/auth/LoginDto';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
-  private baseUrl = 'https://localhost:7161/api';
-  private apiUrl =  'https://localhost:7161/api/auth';
+  // private baseUrl = 'https://localhost:7161/api';
+  // private apiUrl =  'https://localhost:7161/api/auth';
 
   constructor(private http: HttpClient) {}
 
   register(data: RegisterDto) {
-      return this.http.post(  `${this.apiUrl}/register`, data
+      return this.http.post(  `${environment.apiUrl}/register`, data
     );
   }
 
   login(data: LoginDto) {
-    return this.http.post<{ token: string }>( `${this.apiUrl}/login`, data
+    return this.http.post<{ token: string }>( `${environment.apiUrl}/login`, data
     );
   }
 
 
   getBooks() {
-    return this.http.get<BookDto[]>(`${this.baseUrl}/books`);
+    return this.http.get<BookDto[]>(`${environment.baseUrl}/books`);
   }
 
   getBookById(id: number) {
-    return this.http.get<BookDto>(`${this.baseUrl}/books/${id}`);
+    return this.http.get<BookDto>(`${environment.baseUrl}/books/${id}`);
   }
 
   createBook(data: BookDto) {
-    return this.http.post<BookDto>(`${this.baseUrl}/books`, data);
+    return this.http.post<BookDto>(`${environment.baseUrl}/books`, data);
   }
 
   updateBook(id: number, data: BookDto) {
-    return this.http.put<BookDto>(`${this.baseUrl}/books/${id}`, data);
+    return this.http.put<BookDto>(`${environment.baseUrl}/books/${id}`, data);
   }
 
   deleteBook(id: number) {
-    return this.http.delete(`${this.baseUrl}/books/${id}`);
+    return this.http.delete(`${environment.baseUrl}/books/${id}`);
   }
   
 
 
   getAllQuotes() {
-    const data =  this.http.get<QuoteDto[]>(`${this.baseUrl}/quotes`);
+    const data =  this.http.get<QuoteDto[]>(`${environment.baseUrl}/quotes`);
     console.log(data);
     return data;
   }
 
   createQuote(dto: CreateQuoteDto) {
-    return this.http.post<QuoteDto>(`${this.baseUrl}/quotes`, dto);
+    return this.http.post<QuoteDto>(`${environment.baseUrl}/quotes`, dto);
   }
 
   updateQuote(id: number, dto: UpdateQuoteDto) {
-    return this.http.put<QuoteDto>(`${this.baseUrl}/quotes/${id}`, dto);
+    return this.http.put<QuoteDto>(`${environment.baseUrl}/quotes/${id}`, dto);
   }
 
   deleteQuote(id: number) {
-    return this.http.delete(`${this.baseUrl}/quotes/${id}`);
+    return this.http.delete(`${environment.baseUrl}/quotes/${id}`);
     }
 
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://localhost:7161/api/auth',
+  baseUrl: 'https://localhost:7161/api'
+};
