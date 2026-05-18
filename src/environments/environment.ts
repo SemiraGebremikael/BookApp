d855142cb7b455ed88988a0169a6e2ca28f9515f
Semira gebremikael <semiragere61@gmail.com>
2026-05-18 02:49:44 +0200

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://bookapp-backend-f0ewaxhpbucke9ab.westeurope-01.azurewebsites.net/api'
-
+  apiUrl: 'bookapp-backend-f0ewaxhpbucke9ab.westeurope-01.azurewebsites.net'
 };
